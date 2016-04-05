a21e2b6722fd31e5a700648fa872b1b8180a496b
Nick Herrera <nherrera@editllc.com>
2016-04-05 14:37:23 -0700

    chore(build): create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+
+module.exports = {
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		libraryTarget: 'umd',
+		library: 'ngMontage',
+		filename: './dist/angular-montage.js'
+	},
+	resolve: {
+		extensions: ['', '.js']
+	},
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin({minimize: true})
+	]
+}
